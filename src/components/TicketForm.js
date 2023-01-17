c6f93522d70a99735838961e04129f6a8e3fb10e
Henry Unite <henry@lula.is>
2023-01-16 20:23:43 -0500

    feat: require fields

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -8,19 +8,22 @@ export default function TicketForm() {
         size="small"
         color="primary"
         label="Name"
-        variant="filled" />
+        variant="filled"
+        required />
       <TextField
         className="w-full my-1"
         type="email"
         size="small"
         color="primary"
         label="Email"
-        variant="filled" />
+        variant="filled"
+        required />
       <TextField
         label="Description"
         className="w-full my-1"
         multiline
-        rows={4} />
+        rows={4}
+        required />
       <Button
         className="w-full my-1"
         variant="outlined"
